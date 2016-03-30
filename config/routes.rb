72a6e7ae2995d275167cd9a0d67a453a901e9ed8
Mark <mrg007@gmail.com>
2016-03-30 02:02:26 +0000

    editing users and updating

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/home', to: 'pages#home'
   get 'pages/about', to: 'pages#about'
+  get 'signup', to: 'users#new'
 
+  resources :users, except: [:new]
   resources :articles
 
   # Example of regular route:
